7e2ba709d09a7f497e8afa17c32b6063f895b393
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 01:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    map = new Map();
+    for(let i=0;i<str.length;i++){
+        if(map.has(str[i])){
+            map.set(str[i], map.get(str[i])+1);
+        }
+        else{
+            map.set(str[i], 1);
+        }
+    }
+    
+    for(let i=0;i<str.length;i++){
+        if(map.get(str[i])===1){
+            return str[i];
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
